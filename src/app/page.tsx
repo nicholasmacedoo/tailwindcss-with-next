c3fd2206dee38472e383fefab469a8c261dec642
Nicholas Macedo <nicholasmacedoo@gmail.com>
2023-08-03 17:56:12 -0300

    refactor: Slot patten on Input and add Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { SettingsTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
   return (
     <>
-      <h1 className="flex items-center gap-4 font-mono text-2xl font-black before:flex before:h-4 before:w-1 before:w-4 before:rounded-full before:bg-violet-700">
-        Apresentando o Microsoft Defender e o Clipchamp
-      </h1>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
     </>
   )
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,14 @@
-import { Search } from 'lucide-react'
 import { ComponentProps } from 'react'
 
 export type InputPrefixProps = ComponentProps<'div'>
 
-export function InputPrefix(props: InputPrefixProps) {
+export function Prefix(props: InputPrefixProps) {
   return <div {...props} />
 }
 
 export type InputControlProps = ComponentProps<'input'>
 
-export function InputControl(props: InputControlProps) {
+export function Control(props: InputControlProps) {
   return (
     <input
       className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
@@ -20,7 +19,7 @@ export function InputControl(props: InputControlProps) {
 
 export type InputRootProps = ComponentProps<'div'>
 
-export function InputRoot(props: InputRootProps) {
+export function Root(props: InputRootProps) {
   return (
     <div
       className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
